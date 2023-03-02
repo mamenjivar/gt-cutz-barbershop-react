0ecc56f57fab9dbce6c72953fb3d214059766506
Miguel <menjivarmiguel221@gmail.com>
2023-03-01 19:50:11 -0800

    pushing vercel analytics

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Footer from './components/Footer/Footer';
 import NavigationBar from './components/NavigationBar/NavigationBar.tsx';
 import Home from './pages/Home/Home.jsx';
 
+// Vercel analytics
+import { Analytics } from '@vercel/analytics/react';
+
+
 function App() {
   return (
     <Fragment>
       <NavigationBar />
       <Home />
       <Footer />
+      <Analytics  />
     </Fragment>
   );
 }
