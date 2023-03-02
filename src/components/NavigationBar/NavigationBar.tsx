7ec4b5f03b04130d8098272d1ea102ffb88ebfb1
Miguel <menjivarmiguel221@gmail.com>
2023-03-01 20:59:32 -0800

    fixing the a tag bug with nested anchor tags

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -14,38 +14,38 @@ const NavigationBar = () => {
         <Container>
           <Navbar.Brand href="#home">GT CUTZ</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
             <Nav>
-              <Nav.Link>
-                <ScrollLink
-                  to="hours-destination"
-                  spy={true}
-                  smooth={true}
-                  duration={100}
-                >
-                  Hours
-                </ScrollLink>
-              </Nav.Link>
-              <Nav.Link>
-                <ScrollLink
-                  to="location-destination"
-                  spy={true}
-                  smooth={true}
-                  duration={100}
-                >
-                  Location
-                </ScrollLink>
-              </Nav.Link>
-              <Nav.Link>
-                <ScrollLink
-                  to="team-destination"
-                  spy={true}
-                  smooth={true}
-                  duration={100}
-                >
-                  Book Us
-                </ScrollLink>
-              </Nav.Link>
+              <ScrollLink
+                className={classes.navItem}
+                to="hours-destination"
+                spy={true}
+                smooth={true}
+                duration={100}
+              >
+                Hours
+              </ScrollLink>
+              <ScrollLink
+                className={classes.navItem}
+                to="location-destination"
+                spy={true}
+                smooth={true}
+                duration={100}
+              >
+                Location
+              </ScrollLink>
+              <ScrollLink
+                className={classes.navItem}
+                to="team-destination"
+                spy={true}
+                smooth={true}
+                duration={100}
+              >
+                Book Us
+              </ScrollLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
