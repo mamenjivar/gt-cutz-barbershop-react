0c5a2ee73acac1fad6d5961e0b3862726b763e7b
Miguel <menjivarmiguel221@gmail.com>
2023-03-11 22:23:04 -0800

    refactored the team page. Added a data page which holds all barber information, easier to add and remove users

---
+++ src/constants/teamData.js
@@ -0,0 +1,90 @@
+// team images
+import gtcutz from '../img/team-img/Gtctuz-Gt_the_barber.jpeg';
+import dannyhbarber from '../img/team-img/dannybarber.jpeg';
+import lavishcutz from '../img/team-img/LavishhJr_Cutzz-Jr_the_barber.jpeg';
+import miguelblends from '../img/team-img/_miguelblends-Miguel_the_barber.jpeg';
+import ciskoeCutz from '../img/team-img/Ciskoe_Cutz-Ciskoe_the_barber.jpeg';
+import jumpyCutz from '../img/team-img/jumpy_cutz-Hector_the_barber.jpeg';
+import chuckiscutz from '../img/team-img/chuckiscuts1-vanessa-Garza.jpeg';
+import diegocutz from '../img/team-img/Diego-cutz-barber.jpeg';
+import pollocutz from '../img/team-img/pollocutz-barber.jpeg';
+
+/**
+ * Data where all team information is located.
+ */
+const teamData = [
+  {
+    image: gtcutz,
+    name: 'GT the Barber',
+    specialty: '',
+    handleName: '@GTcutz',
+    handleLink: 'https://www.instagram.com/gtcutz/?hl=en',
+    bookLink: 'https://getsqr.co/gt-the-barber',
+  },
+  {
+    image: dannyhbarber,
+    specialty: '',
+    name: 'Danny the Barber',
+    handleName: '@DannyHBarber',
+    handleLink: 'https://www.instagram.com/dannybarberla/?hl=en',
+    bookLink: 'https://dannybarber.square.site/',
+  },
+  {
+    image: lavishcutz,
+    name: 'Jr the Barber',
+    specialty: '',
+    handleName: '@LavishhJr_Cutzz',
+    handleLink: 'https://www.instagram.com/lavishhjr_cutzz/?hl=en',
+    bookLink: 'http://book.thecut.co/lavishhjr_cutzz'
+  },
+  {
+    image: miguelblends,
+    name: 'Miguel the Barber',
+    specialty: '',
+    handleName: '@_Miguelblends',
+    handleLink: 'https://www.instagram.com/_Miguelblends/',
+    bookLink: 'https://getsqr.co/miguel-silva'
+  },
+  {
+    image: ciskoeCutz,
+    name: 'Ciskoe the Barber',
+    specialty: '',
+    handleName: '@Ciskoe_Cutz',
+    handleLink: 'https://www.instagram.com/Ciskoe_Cutz/',
+    bookLink: 'https://instagram.com/ciskoe_cutz?igshid=YmMyMTA2M2Y='
+  },
+  {
+    image: jumpyCutz,
+    name: 'Hector the Barber',
+    specialty: '',
+    handleName: '@Jumpy_Cutz',
+    handleLink: 'https://www.instagram.com/Jumpy_Cutz/',
+    bookLink: 'https://instagram.com/jumpy_cutz?igshid=YmMyMTA2M2Y='
+  },
+  {
+    image: chuckiscutz,
+    name: 'Vanessa',
+    specialty: 'Barber / Cosmetologist',
+    handleName: '@ChuckisCuts1',
+    handleLink: 'https://www.instagram.com/ChuckisCuts1/',
+    bookLink: 'https://instagram.com/chuckiscuts1?igshid=YmMyMTA2M2Y='
+  },
+  {
+    image: diegocutz,
+    name: 'Diego the Barber',
+    specialty: '',
+    handleName: '@d.tb___',
+    handleLink: 'https://www.instagram.com/d.tb___/',
+    bookLink: 'http://book.thecut.co/dtb_'
+  },
+  {
+    image: pollocutz,
+    name: 'Pollo the Barber',
+    specialty: '',
+    handleName: '@pollocutz_',
+    handleLink: 'https://www.instagram.com/pollocutz_/',
+    bookLink: 'https://www.instagram.com/pollocutz_/?igshid=YmMyMTA2M2Y%3D'
+  }
+];
+
+export default teamData;
