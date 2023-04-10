53749975cff448ad21823109d02870fc1250c099
Miguel <menjivarmiguel221@gmail.com>
2023-04-09 19:29:34 -0700

    made events page cards dynamic with external data. and styled the card

--- src/components/EventCards/EventCards.jsx
+++ src/components/EventCards/EventCards.jsx
@@ -6,33 +6,38 @@ import styles from './EventCards.module.scss';
 
 import testImage from '../../img/team-img/dannybarber.jpeg';
 
+import eventsData from '../../constants/eventsData';
+
 const EventCards = () => {
   return (
     <Container>
       <Row>
-        <Col>
-          <Card
-            style={{ width: '50rem', flexDirection: 'row' }}
-            className="mx-auto"
-          >
-            <Card.Img className={styles.imageSize} src={testImage} />
-            <Card.Body className={styles.testBorder}>
-              <Card.Title className={styles.centerTitle}>
-                Barber Event
-              </Card.Title>
-              <Card.Text>
-                <b>June 30, 2023</b>
-                <br />
-                Sign up so you can be a part of the barber event happening at
-                the shop!
-              </Card.Text>
-              <div>
-                <Button>More Info</Button>
-                <Button>Sign Up</Button>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
+        {eventsData.map((item, index) => {
+          return (
+            <Col key={index}>
+              <Card
+                style={{ width: '50rem', flexDirection: 'row' }}
+                className="mx-auto"
+              >
+                <Card.Img className={styles.imageSize} src={item.image} />
+                <Card.Body className="d-flex flex-column">
+                  <Card.Title className={styles.centerTitle}>
+                    {item.eventTitle}
+                  </Card.Title>
+                  <Card.Text>
+                    <b>{item.eventDate}</b>
+                    <br />
+                    {item.eventDescription}
+                  </Card.Text>
+                  <div className="mt-auto d-flex justify-content-around">
+                    <Button className="btn-lg">More Info</Button>
+                    <Button className="btn-lg">Sign Up</Button>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   );
---
+++ src/constants/eventsData.js
@@ -0,0 +1,23 @@
+import dannyhbarber from '../img/team-img/dannybarber.jpeg';
+
+/**
+ * Data where all team information is located.
+ */
+const eventsData = [
+  {
+    image: dannyhbarber,
+    eventTitle: 'Barber Showcase',
+    eventDate: 'April 30, 2023',
+    eventDescription: 'PRESENTING OUR FIRST EVER BARBER SHOWCASE EVER! Open to those who are interested getting into haircutting. ',
+    eventSignUpLink: '#'
+  },
+  {
+    image: dannyhbarber,
+    eventTitle: 'School Drive',
+    eventDate: 'June 30, 2023',
+    eventDescription: 'School drive, come to the shop to pick up school supplies before the year starts!. ',
+    eventSignUpLink: '#'
+  },
+];
+
+export default eventsData;
