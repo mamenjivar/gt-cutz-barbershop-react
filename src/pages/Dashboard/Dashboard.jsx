2873599af2b502512ee1a77fb9f88e5ec91bb9d7
Miguel <menjivarmiguel221@gmail.com>
2024-09-02 21:19:56 -0700

    added label field and added click action to form buttons

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,41 +10,56 @@ import classes from './Dashboard.module.scss';
 const Dashboard = () => {
     // lassName={`${styles.backgroundImage} ${styles.backgroundText} ${styles.backgroundColumns}`}>
     // import classes from './MeetTheTeam.module.scss';
+
+    // sends request to create a new barber
+    const createBarber = () => {
+        console.log('clicked!');
+    }
+
+    // go back to main dashboard page
+    const goBack = () => {
+        console.log('go back clicked');
+    }
+
     return (
         <section >
             <h1 className={classes.header}>Add a new Barber</h1>
-            
 
             <form>
                 <fieldset>
                     <div>
-                        <input placeholder='Name' type='text' id='name' /><br />
+                        <label htmlFor='name'>Name</label><br />
+                        <input type='text' id='name' /><br />
                         <span>The name of the Barber. (This won't get displayed on website)</span>
                     </div>
 
                     <div>
-                        <input placeholder='Barber Name' type='text' id='barberName' /><br />
+                        <label htmlFor='barberName'>Barber Name</label><br />
+                        <input type='text' id='barberName' /><br />
                         <span>Nickname of barber that will get displayed in the website.</span>
                     </div>
 
                     <div>
-                        <input placeholder='Instagram handle' type='text' id='instagram' /><br />
+                        <label htmlFor='instagramHandle'>Instagram Handle</label><br />
+                        <input type='text' id='instagramHandle' /><br />
                         <span>The username for this Barber that appears on Instagram. (optional)</span>
                     </div>
 
                     {/* This field can be constructed using the Instagram Handle above, may need to test */}
                     <div>
-                        <input placeholder="Instagram user's URL" type='text' id='instagramURL' /><br />
+                        <label htmlFor='instagramURL'>Instagram URL</label><br />
+                        <input type='text' id='instagramURL' /><br />
                         <span>Insert the URL for this Barber's Instagram page. (optional)</span>
                     </div>
 
                     <div>
-                        <input placeholder='Booking URL' type='text' id='bookingURL' /><br />
+                        <label htmlFor='bookingURL'>Booking URL</label><br />
+                        <input type='text' id='bookingURL' /><br />
                         <span>Place the link where customers can schedule appointments with this Barber.</span>
                     </div>
 
-                    <button type='button'>Create</button>
-                    <button type='button'>Go Back</button>
+                    <button type='button' onClick={createBarber}>Create</button>
+                    <button type='button' onClick={goBack}>Go Back</button>
                 </fieldset>
             </form>
         </section>
