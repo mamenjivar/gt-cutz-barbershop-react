991af6d9527fff6a6c41b73a1e630cadded3c72f
Miguel <menjivarmiguel221@gmail.com>
2024-09-03 21:32:17 -0700

    made changes to the form, centered the form and extended field width; still needs some more styling

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -25,43 +25,51 @@ const Dashboard = () => {
         <section >
             <h1 className={classes.header}>Add a new Barber</h1>
 
-            <form>
-                <fieldset>
-                    <div>
-                        <label htmlFor='name'>Name</label><br />
-                        <input type='text' id='name' /><br />
-                        <span>The name of the Barber. (This won't get displayed on website)</span>
-                    </div>
+            <div className={classes.createForm}>
+                <form>
+                    <fieldset>
+                        <div>
+                            <label htmlFor='name'>Name</label><br />
+                            <input type='text' id='name' /><br />
+                            <span>The name of the Barber. (This won't get displayed on website)</span>
+                        </div>
 
-                    <div>
-                        <label htmlFor='barberName'>Barber Name</label><br />
-                        <input type='text' id='barberName' /><br />
-                        <span>Nickname of barber that will get displayed in the website.</span>
-                    </div>
+                        <div>
+                            <label htmlFor='barberName'>Barber Name</label><br />
+                            <input type='text' id='barberName' /><br />
+                            <span>Nickname of barber that will get displayed in the website.</span>
+                        </div>
 
-                    <div>
-                        <label htmlFor='instagramHandle'>Instagram Handle</label><br />
-                        <input type='text' id='instagramHandle' /><br />
-                        <span>The username for this Barber that appears on Instagram. (optional)</span>
-                    </div>
+                        <div>
+                            <label htmlFor='instagramHandle'>Instagram Handle</label><br />
+                            <input type='text' id='instagramHandle' /><br />
+                            <span>The username for this Barber that appears on Instagram. (optional)</span>
+                        </div>
 
-                    {/* This field can be constructed using the Instagram Handle above, may need to test */}
-                    <div>
-                        <label htmlFor='instagramURL'>Instagram URL</label><br />
-                        <input type='text' id='instagramURL' /><br />
-                        <span>Insert the URL for this Barber's Instagram page. (optional)</span>
-                    </div>
+                        {/* This field can be constructed using the Instagram Handle above, may need to test */}
+                        <div>
+                            <label htmlFor='instagramURL'>Instagram URL</label><br />
+                            <input type='text' id='instagramURL' /><br />
+                            <span>Insert the URL for this Barber's Instagram page. (optional)</span>
+                        </div>
 
-                    <div>
-                        <label htmlFor='bookingURL'>Booking URL</label><br />
-                        <input type='text' id='bookingURL' /><br />
-                        <span>Place the link where customers can schedule appointments with this Barber.</span>
-                    </div>
+                        <div>
+                            <label htmlFor='bookingURL'>Booking URL</label><br />
+                            <input type='text' id='bookingURL' /><br />
+                            <span>Place the link where customers can schedule appointments with this Barber.</span>
+                        </div>
 
-                    <button type='button' onClick={createBarber}>Create</button>
-                    <button type='button' onClick={goBack}>Go Back</button>
-                </fieldset>
-            </form>
+                        <div>
+                            <label htmlFor='isActive'>Is Barber Active?</label><br />
+                            <input type='checkbox' id='isActive' checked/><br />
+                            <span>Keep box selected so the barber can show on the website.</span>
+                        </div>
+
+                        <button type='button' onClick={createBarber}>Create</button>
+                        <button type='button' onClick={goBack}>Go Back</button>
+                    </fieldset>
+                </form>
+            </div>
         </section>
     );
 };
